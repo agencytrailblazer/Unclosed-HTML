ca0fc56f3697bfcff9ce62646d2e67464ee85793
Lee Matthew Jackson <leematthewjackson@posteo.net>
2024-07-30 09:54:59 +0100

    0.3

--- README.md
+++
@@ -1,2 +0,0 @@
-# Unclosed-HTML
-When clients add their own HTML to a page builder, they can often cause issues. This tool will find any unclosed HTML such as "i" "b" "div" and so on causing layout or font issues.
--- html-checker.js
+++ html-checker.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function($) {
-    $('#html-checker-form').on('submit', function(e) {
+    $('#html-checker-form').submit(function(e) {
         e.preventDefault();
         var url = $('#url').val();
         $('#html-checker-result').html('Checking...');
-
+        
         $.ajax({
-            url: htmlCheckerAjax.ajax_url,
-            type: 'post',
+            url: htmlCssCheckerAjax.ajax_url,
+            type: 'POST',
             data: {
                 action: 'html_checker_check_html',
                 url: url
@@ -16,6 +16,27 @@ jQuery(document).ready(function($) {
             },
             error: function() {
                 $('#html-checker-result').html('An error occurred.');
+            }
+        });
+    });
+
+    $('#negative-margin-checker-form').submit(function(e) {
+        e.preventDefault();
+        var url = $('#negative-margin-url').val();
+        $('#negative-margin-checker-result').html('Checking...');
+        
+        $.ajax({
+            url: htmlCssCheckerAjax.ajax_url,
+            type: 'POST',
+            data: {
+                action: 'negative_margin_checker_check_css',
+                url: url
+            },
+            success: function(response) {
+                $('#negative-margin-checker-result').html(response);
+            },
+            error: function() {
+                $('#negative-margin-checker-result').html('An error occurred.');
             }
         });
     });
