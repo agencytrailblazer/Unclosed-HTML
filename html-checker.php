763ecd1ea728f5fcbe95788059454984cbd3a795
Lee Matthew Jackson <leematthewjackson@posteo.net>
2024-07-30 10:10:44 +0100

    0.4

--- html-checker.php
+++ html-checker.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: HTML and CSS Checker
-Description: A plugin to check for unclosed HTML tags and negative margins on a given URL.
-Version: 0.3
+Plugin Name: Unclosed HTML Checker
+Description: A plugin to check for unclosed HTML tags on a given URL.
+Version: 0.2
 Author: Lee Matthew Jackson
 */
 
@@ -11,13 +11,13 @@ if (!defined('ABSPATH')) {
 }
 
 // Enqueue the script for AJAX
-function html_css_checker_enqueue_script() {
-    wp_enqueue_script('html-css-checker', plugin_dir_url(__FILE__) . 'html-css-checker.js', array('jquery'), null, true);
-    wp_localize_script('html-css-checker', 'htmlCssCheckerAjax', array('ajax_url' => admin_url('admin-ajax.php')));
+function html_checker_enqueue_script() {
+    wp_enqueue_script('html-checker', plugin_dir_url(__FILE__) . 'html-checker.js', array('jquery'), null, true);
+    wp_localize_script('html-checker', 'htmlCheckerAjax', array('ajax_url' => admin_url('admin-ajax.php')));
 }
-add_action('wp_enqueue_scripts', 'html_css_checker_enqueue_script');
+add_action('wp_enqueue_scripts', 'html_checker_enqueue_script');
 
-// Shortcode to display the HTML checker form
+// Shortcode to display the form
 function html_checker_form_shortcode() {
     ob_start();
     ?>
@@ -30,20 +30,6 @@ function html_checker_form_shortcode() {
     return ob_get_clean();
 }
 add_shortcode('html_checker_form', 'html_checker_form_shortcode');
-
-// Shortcode to display the negative margin checker form
-function negative_margin_checker_form_shortcode() {
-    ob_start();
-    ?>
-    <form id="negative-margin-checker-form">
-        <input type="url" name="url" id="negative-margin-url" placeholder="Enter URL" required>
-        <button type="submit">Check Negative Margins</button>
-    </form>
-    <div id="negative-margin-checker-result"></div>
-    <?php
-    return ob_get_clean();
-}
-add_shortcode('negative_margin_checker_form', 'negative_margin_checker_form_shortcode');
 
 // Function to find unclosed HTML tags
 function find_unclosed_tags($html) {
@@ -127,80 +113,3 @@ function html_checker_check_html() {
 }
 add_action('wp_ajax_html_checker_check_html', 'html_checker_check_html');
 add_action('wp_ajax_nopriv_html_checker_check_html', 'html_checker_check_html');
-
-// Function to fetch and parse CSS
-function fetch_and_parse_css($url) {
-    $css_rules = array();
-    
-    // Fetch the HTML
-    $response = wp_remote_get($url);
-    if (is_wp_error($response)) {
-        return false;
-    }
-    $html = wp_remote_retrieve_body($response);
-    
-    // Create a new DOMDocument
-    $dom = new DOMDocument();
-    @$dom->loadHTML($html);
-    
-    // Find all <link> tags with rel="stylesheet"
-    $links = $dom->getElementsByTagName('link');
-    foreach ($links as $link) {
-        if ($link->getAttribute('rel') == 'stylesheet') {
-            $css_url = $link->getAttribute('href');
-            if (strpos($css_url, 'http') !== 0) {
-                $css_url = $url . $css_url;
-            }
-            $css_content = wp_remote_retrieve_body(wp_remote_get($css_url));
-            $css_rules = array_merge($css_rules, parse_css($css_content));
-        }
-    }
-    
-    // Find all <style> tags
-    $styles = $dom->getElementsByTagName('style');
-    foreach ($styles as $style) {
-        $css_rules = array_merge($css_rules, parse_css($style->nodeValue));
-    }
-    
-    return $css_rules;
-}
-
-function parse_css($css_content) {
-    $css_rules = array();
-    preg_match_all('/([^{]+){([^}]+)}/s', $css_content, $matches);
-    
-    foreach ($matches[0] as $i => $match) {
-        $selector = trim($matches[1][$i]);
-        $rules = trim($matches[2][$i]);
-        if (preg_match('/margin(-[a-z]+)?:\s*-[0-9]+[a-z%]*/i', $rules)) {
-            $css_rules[] = array(
-                'selector' => $selector,
-                'rule' => $rules
-            );
-        }
-    }
-    
-    return $css_rules;
-}
-
-// AJAX handler to check for negative margins
-function negative_margin_checker_check_css() {
-    if (isset($_POST['url'])) {
-        $url = esc_url_raw($_POST['url']);
-        $css_rules = fetch_and_parse_css($url);
-        
-        if (empty($css_rules)) {
-            echo 'No negative margins found.';
-        } else {
-            $count = 1;
-            foreach ($css_rules as $rule) {
-                echo "<strong>Issue $count:</strong> Negative margin found in selector \"{$rule['selector']}\"<br>";
-                echo "CSS Rule: <pre>{$rule['rule']}</pre><br>";
-                $count++;
-            }
-        }
-    }
-    wp_die();
-}
-add_action('wp_ajax_negative_margin_checker_check_css', 'negative_margin_checker_check_css');
-add_action('wp_ajax_nopriv_negative_margin_checker_check_css', 'negative_margin_checker_check_css');
